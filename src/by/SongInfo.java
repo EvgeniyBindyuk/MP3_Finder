8ef0ad948f8f96ba708a7d4104691133f51c11b2
Evgeniy Bindyuk <evganius@gmail.com>
2019-03-13 17:40:06 +0400

    reg exp

--- src/by/SongInfo.java
+++ src/by/SongInfo.java
@@ -3,21 +3,22 @@ package by;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.regex.Pattern;
 
 class SongInfo {
 
     void getInfo(File file) throws IOException {
         RandomAccessFile random = new RandomAccessFile(file, "r");
-        for (long i = random.length() - 223; i < random.length() - 163; i++) {
-            byte b = (byte) random.read();
-            String s1 = String.format("%s", Integer.toBinaryString(b & 0xFF)).substring(1);
-            if (!s1.equals("")) {
-                System.out.print((char) Byte.parseByte(s1, 0b10));
+        StringBuilder sb = new StringBuilder();
+        for (long i = 0; i < 400; i++) {
+            char a = (char) random.read();
+            String str = String.valueOf(a);
+            sb.append(str);
+            if (str.matches("[A-Za-zА-Яа-яеЁ0-9\\s]")) {
+                System.out.print(a);
             }
-//            byte b1 = (byte) 129;
-////            String s1 = String.format("%8s", Integer.toBinaryString(b1 & 0xFF)).replace(' ', '0');
-////            System.out.println(s1); // 10000001
+//            String str2 = new String(sb);
+//            Pattern album = Pattern.compile("[TALB]")
         }
     }
-
 }
